702161fc59e21e4dd92c1b5207d630df44e113b5
Kian Hindsgaul Lindskou <kian0949@edu.campusvejle.dk>
2019-05-22 09:59:00 +0200

    Add project files.

---
+++ S2.AspNet.Repetition/Pages/NewMeme.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace S2.AspNet.Repetition.Pages
+{
+    public class NewMemeModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int ImageSelected { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MemeText { get; set; } = "";
+        [BindProperty(SupportsGet = true)]
+        public int TextSize { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int TextColor { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string TextPosition { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MemeName { get; set; }
+        public string TextColorStyle { get; set; }
+        public string SelectedImageUrl { get; set; }
+        public void OnGet()
+        {
+            SelectedImageUrl = $"/img/meme{ImageSelected}.png";
+            switch (TextColor)
+            {
+                case 1:
+                    TextColorStyle = "black";
+                    break;
+                case 2:
+                    TextColorStyle = "white";
+                    break;
+                case 3:
+                    TextColorStyle = "yellow";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
